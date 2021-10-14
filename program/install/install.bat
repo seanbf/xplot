b9fb93b8777a1aa31d866fb9ec5a336b8d9a1077
seanbf <83425701+seanbf@users.noreply.github.com>
2021-10-14 11:54:07 +0100

    Add files via upload

--- program/install/install.bat
+++ program/install/install.bat
@@ -2,9 +2,8 @@
 cls
 :start
 SET "ThisScriptsDirectory=%~dp0\\..\\.."
-SET PowerShellScriptPath=%ThisScriptsDirectory%\program\install\ps_script.ps1
-SET AppName=xPlot
-SET FileName=xplot
+SET AppName=Torque Accuracy Tool
+SET FileName=torque_accuracy_tool
 ECHO.
 ECHO ##### %AppName% Install #####
 ECHO. 
@@ -14,7 +13,7 @@ ECHO    [2] Install Dependancies and Launch %AppName%
 ECHO. 
 
 set choice=
-set /p choice=Type the [number] of the option you want to use.
+set /p choice=Type the [number] of the option you want to use: 
 ECHO. 
 
 if not '%choice%'=='' set choice=%choice:~0,1%
@@ -26,14 +25,33 @@ ECHO.
 goto start
 
 :install_full
-ECHO Installing Python 3.7.4, Paths, Dependancies and Launching %AppName%
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs}"
+
+ECHO Downloading Python Installer (python-3.7.4-amd64.exe)
+ECHO.
+
+curl "https://www.python.org/ftp/python/3.7.4/python-3.7.4-amd64.exe" -o python-3.7.4-amd64.exe
+
+ECHO.
+ECHO Installing Python (python-3.7.4-amd64.exe)
+ECHO.
+
+python-3.7.4-amd64.exe /quiet InstallAllUsers=0 PrependPath=1 AssociateFiles=0
+
+ECHO Refreshing Environmental Variables
+ECHO.
+
+call %ThisScriptsDirectory%\program\install\RefreshEnv.cmd
+
+del python-3.7.4-amd64.exe
+
 goto install_dep
 
 :install_dep
-ECHO Installing Dependancies and Launching %AppName%
 
+ECHO.
 ECHO Generating Desktop Shortcut and Icon
+ECHO.
+
 set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
 echo sLinkFile = "%USERPROFILE%\Desktop\%AppName%.lnk" >> %SCRIPT%
@@ -45,9 +63,14 @@ echo oLink.Save >> %SCRIPT%
 cscript /nologo %SCRIPT%
 del %SCRIPT%
 
-py -3.7 -m ensurepip
-py -3.7 -m pip install --user --upgrade pip
-py -3.7 -m pip install -r %ThisScriptsDirectory%\program\install\requirements.txt
-py -3.7 -m streamlit run %ThisScriptsDirectory%\program\%FileName%.py
+ECHO Installing Dependancies
+ECHO.
 
-pause
+py -3.7 -m pip install --upgrade pip
+py -3.7 -m pip install -r %ThisScriptsDirectory%\program\install\requirements.txt
+
+ECHO.
+ECHO Launching %AppName%
+ECHO.
+
+py -3.7 -m streamlit run %ThisScriptsDirectory%\program\%FileName%.py
